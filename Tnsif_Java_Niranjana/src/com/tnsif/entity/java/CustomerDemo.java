4ac697487561a9cac49a0d93b0650bdaba6ba5a5
HP <HP@DESKTOP-MVLOA4R>
2025-09-26 20:44:18 +0530

    COMMIT1

---
+++ Tnsif_Java_Niranjana/src/com/tnsif/entity/java/CustomerDemo.java
@@ -0,0 +1,21 @@
+package com.tnsif.entity.java;
+
+public class CustomerDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Customer c1=new Customer();
+		c1.setId(101);
+		c1.setName("raj");
+		c1.setCity("Chennai");
+		System.out.println(c1);
+		
+		Customer c2=new Customer();
+		c2.setId(102);
+		c2.setName("ram");
+		c2.setCity("Chennai");
+		System.out.println(c2);
+
+	}
+
+}

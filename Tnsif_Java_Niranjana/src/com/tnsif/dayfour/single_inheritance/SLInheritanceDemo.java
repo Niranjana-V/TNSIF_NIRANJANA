016798b6800971f7f62a47c1b72572ef863c2598
HP <HP@DESKTOP-MVLOA4R>
2025-09-30 22:21:57 +0530

    commit2

---
+++ Tnsif_Java_Niranjana/src/com/tnsif/dayfour/single_inheritance/SLInheritanceDemo.java
@@ -0,0 +1,17 @@
+package com.tnsif.dayfour.single_inheritance;
+
+
+
+public class SLInheritanceDemo {
+
+	
+		// TODO Auto-generated method stub
+		public static void main(String[] args) {
+			
+			Student student = new Student("Gayatri", "8080808080", "Bangalore", 9888878787L, 10, "City College");
+			System.out.println(student);
+		}
+
+	}
+
+

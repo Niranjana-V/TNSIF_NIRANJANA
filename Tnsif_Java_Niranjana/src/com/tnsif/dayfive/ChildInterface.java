e5d34738d2db9fe1e81ca30578779a3f092e093a
HP <HP@DESKTOP-MVLOA4R>
2025-10-09 15:02:37 +0530

    commit

---
+++ Tnsif_Java_Niranjana/src/com/tnsif/dayfive/ChildInterface.java
@@ -0,0 +1,5 @@
+package com.tnsif.dayfive;
+
+public interface ChildInterface extends InterfaceOne {
+	void show();
+}
---
+++ Tnsif_Java_Niranjana/src/com/tnsif/dayfive/InterfaceOne.java
@@ -0,0 +1,5 @@
+package com.tnsif.dayfive;
+
+interface InterfaceOne{  
+	  void print();  
+	}  
